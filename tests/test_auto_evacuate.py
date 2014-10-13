0dfed8f8a86c6664ba026dce61b67406a1a9c729
atoato88 <atoato88@example.com>
2014-10-13 23:23:41 +0900

    add some tests.

--- tests/test_auto_evacuate.py
+++ tests/test_auto_evacuate.py
@@ -8,6 +8,7 @@ import mox
 import auto_evacuate
 import ConfigParser
 import optparse
+from pyzabbix import ZabbixAPI
 
 class AutoEvacuateTest(unittest.TestCase):
 
@@ -27,7 +28,6 @@ class AutoEvacuateTest(unittest.TestCase):
     self.mox.VerifyAll()
 
   def test_parse_args_NG(self):
-    self.mox.CreateMock(optparse.OptionParser)
     self.mox.StubOutWithMock(optparse.OptionParser, 'parse_args')
     a = optparse.Values()
     a.dry_run = True
@@ -40,7 +40,58 @@ class AutoEvacuateTest(unittest.TestCase):
   def test_load_config_OK(self):
     self.mox.ReplayAll()
     ret = auto_evacuate.load_config()
-    print ret
+    #print ret
+    self.mox.VerifyAll()
+
+  def test_load_config_OK_with_zabbix(self):
+    self.mox.StubOutWithMock(ConfigParser.ConfigParser, 'getboolean')
+    ConfigParser.ConfigParser().getboolean(mox.IgnoreArg(), mox.IgnoreArg()).AndReturn(True)
+    # above is dummy for ConfigParser.ConfigParser().getboolean('DEFAULT', 'evacuate_with_shared_storage').AndReturn(True)
+    ConfigParser.ConfigParser().getboolean('DEFAULT', 'zabbix_comment_update').AndReturn(True)
+    ConfigParser.ConfigParser().getboolean(mox.IgnoreArg(), mox.IgnoreArg()).AndReturn(True)
+    # above is dummy for ConfigParser.ConfigParser().getboolean('DEFAULT', 'ignore_zabbix_api_connection').AndReturn(True)
+
+    self.mox.StubOutWithMock(auto_evacuate, 'get_zabbix_api')
+    auto_evacuate.get_zabbix_api().AndReturn('dummy return')
+
+    self.mox.ReplayAll()
+    ret = auto_evacuate.load_config()
+    self.assertEqual(ret['zapi'], 'dummy return')
+    self.mox.VerifyAll()
+
+  def test_acknowledge_OK_without_zabbix(self):
+    self.mox.StubOutWithMock(auto_evacuate, 'conf')
+    auto_evacuate.conf['zabbix_comment_update'].AndReturn(False)
+
+    self.mox.ReplayAll()
+    auto_evacuate.acknowledge('dummy zabbix id', 'dummy message')
+    self.mox.VerifyAll()
+
+  def test_acknowledge_OK_with_zabbix(self):
+    self.mox.StubOutWithMock(auto_evacuate, 'conf')
+    auto_evacuate.conf['zabbix_comment_update'].AndReturn(True)
+    auto_evacuate.conf['zabbix_comment_update'].AndReturn(True)
+    auto_evacuate.conf['ignore_zabbix_api_connection'].AndReturn(True)
+    zapi = mox.MockAnything()
+    zapi.event = mox.MockAnything()
+    zapi.event.acknowledge(mox.IgnoreArg(), mox.IgnoreArg()).AndReturn(None)
+
+    self.mox.ReplayAll()
+    auto_evacuate.acknowledge('dummy zabbix id', 'dummy message')
+    self.mox.VerifyAll()
+
+  def test_acknowledge_NG_with_zabbix(self):
+    self.mox.StubOutWithMock(auto_evacuate, 'conf')
+    auto_evacuate.conf['zabbix_comment_update'].AndReturn(True)
+    auto_evacuate.conf['zabbix_comment_update'].AndReturn(True)
+    auto_evacuate.conf['ignore_zabbix_api_connection'].AndReturn(False)
+    zapi = mox.MockAnything()
+    zapi.event = mox.MockAnything()
+    zapi.acknowledge(mox.IgnoreArg(), mox.IgnoreArg()).AndRaise(Exception('dummy exception'))
+
+    self.mox.ReplayAll()
+    with self.assertRaises(Exception):
+      auto_evacuate.acknowledge('dummy zabbix id', 'dummy message')
     self.mox.VerifyAll()
 
 if __name__ == '__main__':
